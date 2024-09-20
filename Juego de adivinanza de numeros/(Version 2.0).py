ca9a7e5fc185ae5777de117ca2e87fcc46105f42
joaquin-2006 <151161950+joaquin-2006@users.noreply.github.com>
2024-09-20 14:59:42 -0300

    Add files via upload

---
+++ Juego de adivinanza de numeros/(Version 2.0).py
@@ -0,0 +1,23 @@
+import random
+
+Numero_secreto = random.randint(1, 100)
+intentos = 0
+
+print("!Bienvenido al juego de adivinanza¡")
+print("He pensado en un numero entre 1 y 100")
+print("Tiene que adivinar cual es")
+
+while True:
+    try:
+        Numero_eligido = int(input("¿Que numero pense?: "))
+        intentos += 1
+        
+        if Numero_eligido < Numero_secreto:
+            print("El numero es mayor")
+        elif Numero_eligido > Numero_secreto:
+            print("El numero es menor")
+        elif Numero_eligido == Numero_secreto:
+            print(f"¡Felicidades, adivinaste el numero en un total de: {intentos}")
+            break
+    except ValueError:
+        print("No es un numero valido")
